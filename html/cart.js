5527d0ec442c2648edb06d66fb7607ac647b4651
Kritam <thekritamkhatiwada@gmail.com>
2024-10-06 15:01:50 +0545

    format cart

--- html/cart.js
+++ html/cart.js
@@ -1,11 +1,10 @@
-import { products } from "../html/products.js";
+import { products } from '../html/products.js'
 
-export let cart = JSON.parse(localStorage.getItem('cart')) || [
-];
+export let cart = JSON.parse(localStorage.getItem('cart')) || []
 
-let cartHTML = "";
+let cartHTML = ''
 cart.forEach(x => {
-    cartHTML += `<div class="product" data-product-id="${x.id}">
+  cartHTML += `<div class="product" data-product-id="${x.id}">
                 <div class="image">
                 <img class="img" src="${x.image}" alt="Product Image">
                 </div>
@@ -18,44 +17,46 @@ cart.forEach(x => {
                     <br><br>
                 </div>
                 </div>`
-});
-document.querySelector(".totalHTML").innerHTML = cartHTML;
+})
+document.querySelector('.totalHTML').innerHTML = cartHTML
 
-export function addToCart(event) {
-    const ProductId = event.target.closest('.productParent').querySelector('.products').dataset.productId;
-    const Product = products.find(product => product.id === ProductId);
+export function addToCart (event) {
+  const ProductId = event.target
+    .closest('.productParent')
+    .querySelector('.products').dataset.productId
+  const Product = products.find(product => product.id === ProductId)
 
-    const dropDownElement = event.target.closest('.content').querySelector('.select');
-    let selectedValue = Number(dropDownElement.value);
+  const dropDownElement = event.target
+    .closest('.content')
+    .querySelector('.select')
+  let selectedValue = Number(dropDownElement.value)
 
-    const matchingProduct = cart.find(x => x.id === Product.id)
-    if (matchingProduct) {
-        matchingProduct.quantity += selectedValue;
-    } else if (Product) {
-        Product.quantity = selectedValue;
-        cart.push(Product);
-        console.log("pushed");
-    }
-    localStorage.setItem('cart', JSON.stringify(cart));
-};
+  const matchingProduct = cart.find(x => x.id === Product.id)
+  if (matchingProduct) {
+    matchingProduct.quantity += selectedValue
+  } else if (Product) {
+    Product.quantity = selectedValue
+    cart.push(Product)
+    console.log('pushed')
+  }
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 
-const deleteBTNs = document.querySelectorAll('.delete');
+const deleteBTNs = document.querySelectorAll('.delete')
 deleteBTNs.forEach(x => {
-    x.addEventListener("click", (e) => {
-        const nearProduct = e.target.closest('.product')
-        const ProductId = nearProduct.dataset.productId;
-        console.log(nearProduct.dataset)
-        const Filtercart = cart.filter(x => x.id !== ProductId)
-        console.log(Filtercart)
-        nearProduct.remove();
-        localStorage.setItem('cart', JSON.stringify(Filtercart));
-        if (Filtercart.length === 0) {
-            document.querySelector(".totalHTML").innerHTML = "<p>Your cart is empty.</p>";
-        }
-        // Update the cart variable
-        cart = Filtercart;
-
-    })
-});
-
-
+  x.addEventListener('click', e => {
+    const nearProduct = e.target.closest('.product')
+    const ProductId = nearProduct.dataset.productId
+    console.log(nearProduct.dataset)
+    const Filtercart = cart.filter(x => x.id !== ProductId)
+    console.log(Filtercart)
+    nearProduct.remove()
+    localStorage.setItem('cart', JSON.stringify(Filtercart))
+    if (Filtercart.length === 0) {
+      document.querySelector('.totalHTML').innerHTML =
+        '<p>Your cart is empty.</p>'
+    }
+    // Update the cart variable
+    cart = Filtercart
+  })
+})
