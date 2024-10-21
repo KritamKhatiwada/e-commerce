ed6dc70d42225e1cff682ce7cd8408b14a7c992b
Kritam Nitro <thekritamkhatiwada@gmail.com>
2024-10-21 20:36:30 +0545

    made function for bussiness and normal cart

--- html/cart-oop2.js
+++ html/cart-oop2.js
@@ -1,27 +1,11 @@
-import { products } from "../html/products.js";
+import { products } from "./products.js";
 
-function Cart(LocalStorageKey){
-     const cart = {
-    cartItems : JSON.parse(localStorage.getItem(LocalStorageKey)) || [
-        {
-            id: "1",
-            name: "shoe",
-            image: "images/products/knit-athletic-sneakers-gray.jpg",
-            price: 1200,
-            quantity: 1,
-          },
-          {
-            id: "2",
-            name: "shirt",
-            image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-            price: 1000,
-            quantity: 1,
-          },
-    ],
+export let cartItems = JSON.parse(localStorage.getItem("cart-oop")) || [];
 
- renderCart() {
+export const cart = {
+  renderCart() {
     let cartHTML = "";
-    this.cartItems.forEach((x) => {
+    cartItems.forEach((x) => {
       cartHTML += `<div class="product" data-product-id="${x.id}">
                 <div class="image">
                 <img class="img" src="${x.image}" alt="Product Image">
@@ -42,7 +26,7 @@ function Cart(LocalStorageKey){
   addToCart(event) {
     const ProductId = event.target
       .closest(".productParent")
-      .querySelector(".products").dataset.product   Id;
+      .querySelector(".products").dataset.productId;
     const Product = products.find((product) => product.id === ProductId);
 
     const dropDownElement = event.target
@@ -50,15 +34,15 @@ function Cart(LocalStorageKey){
       .querySelector(".select");
     let selectedValue = Number(dropDownElement.value);
 
-    const matchingProduct = this.cartItems.find((x) => x.id === Product.id);
+    const matchingProduct = cartItems.find((x) => x.id === Product.id);
     if (matchingProduct) {
       matchingProduct.quantity += selectedValue;
     } else if (Product) {
       Product.quantity = selectedValue;
-      this.cartItems.push(Product);
+      cartItems.push(Product);
       console.log("pushed");
     }
-    localStorage.setItem(LocalStorageKey, JSON.stringify(this.cartItems));
+    localStorage.setItem(LocalStorageKey, JSON.stringify(cartItems));
   },
 
   removeFromCart() {
@@ -67,25 +51,17 @@ function Cart(LocalStorageKey){
       x.addEventListener("click", (e) => {
         const nearProduct = e.target.closest(".product");
         const ProductId = nearProduct.dataset.productId;
-        const Filtercart = this.cartItems.filter((x) => x.id !== ProductId);
+        const Filtercart = cartItems.filter((x) => x.id !== ProductId);
         localStorage.setItem(LocalStorageKey, JSON.stringify(Filtercart));
         nearProduct.remove();
-        this.cartItems = Filtercart;
+        cartItems = Filtercart;
       });
     });
   },
 };
-return cart;
-}
 
-export const cart=Cart();
-
+//  const cart= Cart('cart-oop')
+//  const bussinessCart= Cart('bussiness-cart')
 cart.renderCart();
 cart.removeFromCart();
 
- const normalCart= Cart('cart-oop')
- const bussinessCart= Cart('bussiness-cart')
-
- 
- console.log(bussinessCart)
- console.log(normalCart)
--- html/script.js
+++ html/script.js
@@ -1,43 +1,43 @@
 import { products } from "../html/products.js";
-import { cart } from "../html/cart-oop.js";
-import { cartItems } from "../html/cart-oop.js";
+import { cart } from "./cart-oop.js";
+
 
 let productsHTML = "";
 products.forEach((x) => {
   productsHTML += `
-                    <div class=productParent><div class="products" data-product-id="${x.id}">
-
-                    <div class="image">
-                        <img class="img"src="${x.image}" alt="">
-                    </div>
-                    <div class="content">
-                    <div class="productName">${x.name}</div>
-                    <div class="productPrice">Rs.${x.price}</div>
-                    <select name="number"  class="select" >
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                    </select>
-                    <button class="addtocart" >Add To Cart</button>
-                    </div>
-                </div>
-                </div>`;
+  <div class=productParent><div class="products" data-product-id="${x.id}">
+  
+  <div class="image">
+  <img class="img"src="${x.image}" alt="">
+  </div>
+  <div class="content">
+  <div class="productName">${x.name}</div>
+  <div class="productPrice">Rs.${x.price}</div>
+  <select name="number"  class="select" >
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+  <option value="6">6</option>
+  <option value="7">7</option>
+  <option value="8">8</option>
+  <option value="9">9</option>
+  <option value="10">10</option>
+  </select>
+  <button class="addtocart" >Add To Cart</button>
+  </div>
+  </div>
+  </div>`;
 });
 document.querySelector(".totalHTML").innerHTML = productsHTML;
 
 const buttons = document.querySelectorAll(".addtocart");
-
 buttons.forEach((btn) => {
   btn.addEventListener("click", (e) => {
+    console.log
     cart.addToCart(e);
-
+    
     //for cart Quantity
     const selectElement = e.target.closest(".content").querySelector(".select");
     let selectedValue = Number(selectElement.value);
@@ -51,9 +51,8 @@ export function cartMath(x) {
   // console.log(cartQuantity)
   document.getElementById("cartQuantity").innerHTML = cartQuantity;
 }
-
- const filterButton=document.querySelector(".filter");
- const DoneButton=document.querySelector(".Done");
+const filterButton=document.querySelector(".filter");
+const DoneButton=document.querySelector(".Done");
 
 filterButton.addEventListener("click",()=>{
   document.querySelector(".filterpage").style.display="block";
